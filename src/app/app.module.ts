000e654d0a5dd4bbfabee3b61a362cea10d60e6d
L-Law-Liet <a.kadirov.17.06@gmail.com>
2021-02-04 08:20:59 +0500

    ..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddRecordComponent } from './add-record/add-record.component';
+import { ResultsComponent } from './results/results.component';
+import { ResultsHeaderComponent } from './results/results-header/results-header.component';
+import { ResultsBodyComponent } from './results/results-body/results-body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddRecordComponent,
+    ResultsComponent,
+    ResultsHeaderComponent,
+    ResultsBodyComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/results/results-body/results-body.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-results-body',
+  templateUrl: './results-body.component.html',
+  styleUrls: ['./results-body.component.sass']
+})
+export class ResultsBodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
